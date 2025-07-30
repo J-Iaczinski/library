88b1e1376bab6e85aa5582e4a267946d60509545
jonatan <jonatan@Jonatan.>
2025-07-29 21:20:19 -0300

    Main functions added, modal and event listeners added as well

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
 const myLibrary = [];
 
+const createBook = document.getElementById("plus");
+const dialog = document.querySelector("dialog");
+const cancel = document.querySelector("#cancel");
+
+if (createBook) {
+  createBook.addEventListener("click", (e) => {
+    dialog.showModal();
+  });
+}
+
+if (cancel) {
+  cancel.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+  });
+}
+
 // Creates the Book obj
-function Book(title, author, year, status) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
-  this.year = year;
+  this.pages = pages;
   this.status = status;
   this.id = crypto.randomUUID();
 }
@@ -22,3 +39,36 @@ const newBook3 = new Book("Teste 3", "Autor 3", 1999, true);
 addBookToLibrary(newBook);
 addBookToLibrary(newBook2);
 addBookToLibrary(newBook3);
+
+function displayBooks(e) {
+  const container = document.querySelector(".card-container");
+  const createCard = document.querySelector(".card.create");
+
+  const card = document.createElement("div");
+  card.className = "card";
+
+  const title = document.createElement("div");
+  title.className = "title";
+  title.textContent = e.title;
+
+  const author = document.createElement("div");
+  author.className = "author";
+  author.textContent = e.author;
+
+  const rodape = document.createElement("div");
+  rodape.className = "rodape";
+
+  const pages = document.createElement("div");
+  pages.className = "pages";
+  pages.textContent = e.pages;
+
+  const status = document.createElement("div");
+  status.className = "status";
+  status.textContent = e.status;
+
+  rodape.append(pages, status);
+
+  card.append(title, author, rodape);
+
+  container.insertBefore(card, createCard);
+}
